5a6e430ad0bf6d175478fe188f5ed428a0f0205e
klover <joelkerbusch@gmail.com>
2024-08-21 21:08:23 +0200

    Login page

--- src/tsx/Auth/Login.tsx
+++ src/tsx/Auth/Login.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import TopBar from "../Topbar";
+import config from '../../config.json';
+
+
+interface Response {
+    error?: string;
+    message?: string;
+    token?: string;
+}
 
 export default function Login() {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target;
@@ -19,6 +28,42 @@ export default function Login() {
         e.preventDefault();
         console.log("Username:", username);
         console.log("Password:", password);
+
+        const fetchLogin = async () => {
+            try {
+                const result = await fetch(config.api_url + "/auth/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        "username_or_email": username,
+                        "password": password
+                    })
+                });
+
+                const response: Response = await result.json();
+                console.log(response);
+
+                if (response.message) {
+                    console.log("a message");
+                } else if (response.token) {
+                    console.log("a token");
+                    localStorage.setItem("token", response.token)
+                } else if (response.error) {
+                    console.log("error message");
+                    setError(response.error);
+                } else {
+                    console.log("unknown message type");
+                }
+                
+                
+            } catch (e) {
+                setError(String(e));
+            }
+        }
+
+        fetchLogin();
     };
 
 
@@ -54,6 +99,9 @@ export default function Login() {
                         <br/>
                         <input type="submit" name="login" value="login"></input>
                     </form>
+                    <div className="error-msg-container">
+                        <h4 className="error-msg-container-h4">{error}</h4>
+                    </div>
                 </div>
             </div>
         </div>
