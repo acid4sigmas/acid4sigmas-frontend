183eff7e731af5ffa35ec29563027c5ffa272cf5
klover <joelkerbusch@gmail.com>
2024-08-22 21:20:53 +0200

    logout button added

--- src/tsx/Settings.tsx
+++ src/tsx/Settings.tsx
@@ -1,8 +1,27 @@
 import { Link } from "react-router-dom";
 import TopBar from "./Topbar";
 import "../style/Settings.scss";
+import { useEffect, useState } from "react";
 
 export default function Settings() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const checkIfLoggedIn = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoggedIn(true);
+    } 
+  }
+
+  useEffect(() => {
+    checkIfLoggedIn();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+  }
+
   return (
     <div>
       <TopBar />
@@ -12,6 +31,13 @@ export default function Settings() {
             <div className="settings-navlink-container">
               <div className="settings-navlink">
                 <Link to="/settings/themes">Themes</Link>
+                {loggedIn ? (
+                <div className="logout-btn">
+                  <button onClick={handleLogout}>Logout</button>
+                </div>
+                ) : (
+                <div></div>
+                )}
               </div>
             </div>
             <div className="settings-content">
