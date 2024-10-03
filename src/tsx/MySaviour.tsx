46ddd0b714e87068f36435a7a929f51800121521
klover-rs <joelkerbusch@gmail.com>
2024-10-03 12:20:34 -0700

    add background image in MySaviour.tsx

--- src/tsx/MySaviour.tsx
+++ src/tsx/MySaviour.tsx
@@ -2,13 +2,19 @@
 
 import { Container } from "../components/Container";
 import TopBar from "./Topbar"
+import cross_bg from '../assets/crosses.png'
 
 export default function MySaviour() {
     return (
-    <div>
+    <div className="min-h-screen w-full" style={{
+        backgroundImage: `url(${cross_bg})`,  
+        backgroundSize: "cover",             
+        backgroundPosition: "center",         
+      }}>
         <TopBar />
         <Container>
-            <h1>Hello world!</h1>
+            <div className="w-[100%] h-full">
+            </div>
         </Container>
     </div>
     );
